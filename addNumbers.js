b3c62fdff24516e398a76ffcfc9646d460a3d0bc
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-12-06 15:10:49 -0500

    Finish addNumbers

--- addNumbers.js
+++ addNumbers.js
@@ -1,6 +1,23 @@
 const readline = require("readline")
-const rl = readline.createInterface({
-    input: process.stdin, 
+ const reader = readline.createInterface({
+    input: process.stdin,
     output: process.stdout
-})
+ })
+
+ function addNumbers(sum, numleft, completionCallback){
+    if (numleft > 0){
+        reader.question ('Enter new number', function(num){
+            const number = parseInt(num);
+        
+        sum += number ;
+        console.log(sum);
+        addNumbers(sum, numleft - 1, completionCallback)
+        })
+    }
+    else if(numleft === 0){
+        completionCallback(sum);
+        reader.close
+    }
+ }
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
 
